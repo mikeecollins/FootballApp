27e0532a66ee2d05d3cccc67d6b0225b146ca9ba
Your Name <you@example.com>
2024-11-19 16:20:53 +0000

    refactored menu

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,30 +5,31 @@ import java.lang.System.exit
 fun main() {
     runMenu()
 }
-fun mainMenu() : Int {
-    println("")
-    println("--------------------")
-    println("COLLEGE FOOTBALL APP")
-    println("--------------------")
-    println("MAIN MENU")
-    println("  1) Add a team")
-    println("  2) List all teams")
-    println("  3) Update a team")
-    println("  4) Delete a team")
-    println("  5) Have this team won the league?") //true or false question boolean
-    println("  6) Add a player")
-    println("  7) List all players")
-    println("  8) Update a player")
-    println("  9) Delete a player")
-    println("  10) How many points those the team have")
-
-
-
-    println("--------------------")
-    println("  0) Exit")
-    println("--------------------")
-    print("==>> ")
-    return readlnOrNull()?.toIntOrNull() ?: -1
+fun mainMenu(): Int {
+    print("""
+          ////////////////\\\\\\\\\\\\\\\\\\
+          |       COLLEGE FOOTBALL APP     |
+          ----------------------------------
+          |  TEAM MENU                     |
+          |--------------------------------|
+          |   1) Add a team                |
+          |   2) List all teams            |
+          |   3) Update a team             |
+          |   4) Delete a team             |
+          |   5) How many trophies won     |
+          |--------------------------------|
+          |  PLAYER MENU                   |
+          |--------------------------------|
+          |   6) Add a player              |
+          |   7) List all players          |
+          |   8) Update a players          |
+          |   9) Delete a players          |
+          |   10) Kit numbers              |
+          ----------------------------------
+          |   0) Exit                      |
+          ////////////////\\\\\\\\\\\\\\\\\\
+          ==>> """.trimMargin(">"))
+    return readln().toInt()
 }
 fun runMenu() {
     do {
@@ -38,12 +39,12 @@ fun runMenu() {
             2  -> listTeam()
             3  -> updateTeams()
             4  -> deleteTeams()
-            5  -> leagueWon()
+            5  -> trophyWon()
             6  -> addPlayer()
             7  -> listPlayer()
             8  -> updatePlayer()
             9  -> deletePlayer()
-            10 -> addPoints()
+            10 -> kitNumbers()
             0  -> exitApp()
             else -> println("Invalid option entered: " + option)
         }
@@ -65,7 +66,7 @@ fun deleteTeams(){
     println("You have selected to delete a team")
 }
 
-fun leagueWon(){
+fun trophyWon(){
     println("You have selected to see if a team has won the league")
 }
 
@@ -83,7 +84,7 @@ fun deletePlayer(){
     println("You have selected to delete a player")
 }
 
-fun addPoints(){
+fun kitNumbers(){
     println("You have selected to add points")
 }
 
