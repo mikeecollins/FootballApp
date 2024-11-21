510aa480c3669fe5a1c809d972eab1391da6c6db
Your Name <you@example.com>
2024-11-21 07:41:21 +0000

    Added models package and Football class

---
+++ src/main/kotlin/models/Football.kt
@@ -0,0 +1,11 @@
+package org.example.models
+
+data class Football ( val Teamdivision: String,
+    val Teamsponsor: String,
+    val Teampoints: Int,
+    val Firstname: String,
+    val Lastname: String,
+    val Playernumber: Int,
+    val isTitleWon: Boolean)
+
+
